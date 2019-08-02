7e33b7800399c5673c738d8885208bb02dec482a
jilikarter <benjamin.slomianowski@gmail.com>
2019-08-02 13:53:00 +0200

    Update env.js

--- src/env.js
+++ src/env.js
@@ -4,3 +4,6 @@ export const DATABASE_URL=process.env.REACT_APP_DATABASE_URL
 export const PROJECT_ID=process.env.REACT_APP_PROJECT_ID
 export const MESSAGING_SENDER_ID=process.env.REACT_APP_MESSAGING_SENDER_ID
 export const APP_ID=process.env.REACT_APP_APP_ID
+
+export const ADMIN_ACCESS=process.env.REACT_APP_ADMIN_ACCESS
+export const VISITOR_ACCESS=process.env.REACT_APP_VISITOR_ACCESS
