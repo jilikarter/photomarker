123b3e4d77da75393f35571e2c32eb4229eb622f
jilikarter <benjamin.slomianowski@gmail.com>
2019-08-02 08:45:28 +0200

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
-export const API_KEY=process.env.API_KEY
-export const AUTH_DOMAIN=process.env.AUTH_DOMAIN
-export const DATABASE_URL=process.env.DATABASE_URL
-export const PROJECT_ID=process.env.PROJECT_ID
-export const MESSAGING_SENDER_ID=process.env.MESSAGING_SENDER_ID
-export const APP_ID=process.env.APP_ID
+export const API_KEY=process.env.REACT_APP_API_KEY
+export const AUTH_DOMAIN=process.env.REACT_APP_AUTH_DOMAIN
+export const DATABASE_URL=process.env.REACT_APP_DATABASE_URL
+export const PROJECT_ID=process.env.REACT_APP_PROJECT_ID
+export const MESSAGING_SENDER_ID=process.env.REACT_APP_MESSAGING_SENDER_ID
+export const APP_ID=process.env.REACT_APP_APP_ID
